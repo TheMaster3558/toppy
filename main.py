b062d390e167b6bd2887a9cd648984acb1ebe965
chawkk6404 <chawk_k@outlook.com>
2022-03-01 19:01:01 -0800

    Untrack files in .gitignore

--- main.py
+++
@@ -1,6 +0,0 @@
-from top_gg import AutoPostHTTPClient
-import asyncio
-
-client = AutoPostHTTPClient('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijg4ODg5NTczODkwODYzOTIzMiIsImJvdCI6dHJ1ZSwiaWF0IjoxNjM1NjEzMDQ0fQ.h5YnWdg1CmtA69Tu-GjujuQeuyGATOQmHt49wQBGFqg',
-                            '888895738908639232')
-

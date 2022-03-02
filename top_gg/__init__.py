39c375c7116593f877e1fc13687fcbaaa5911a47
chawkk6404 <chawk_k@outlook.com>
2022-03-01 19:30:44 -0800

    Package info

--- top_gg/__init__.py
+++ top_gg/__init__.py
@@ -1,1 +1,24 @@
+"""
+top_gg
+-------------------------------
+A simple API wrapper for Top.gg
+
+:copyright: (c) 2022-present The Master
+:license: MIT
+"""
+
+
 from .client import TopGGClient
+from .user import User
+from .bot import Bot
+
+
+__title__ = 'top_gg'
+__author__ = 'The Master'
+__license__ = 'MIT'
+__version__ = '1.0.0'
+
+
+
+
+

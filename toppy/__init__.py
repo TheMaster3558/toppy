ab1860ddc66295c6876f146f6438494dfce9f8c3
chawkk6404 <chawk_k@outlook.com>
2022-04-08 20:58:36 -0700

    Documentation and versions

--- toppy/__init__.py
+++ toppy/__init__.py
@@ -12,3 +12,19 @@ from .client import Client
 from .errors import HTTPException, BadRequest, Unauthorized, Forbidden, RateLimited, ClientNotReady
 
 
+__all__ = (
+    'Client',
+    'HTTPException',
+    'BadRequest',
+    'Unauthorized',
+    'Forbidden',
+    'RateLimited',
+    'ClientNotReady'
+)
+
+
+__title__ = 'toppy'
+__authors__ = ('The Master', 'chawkk6404')
+__license__ = 'MIT'
+__copyright__ = '2022-present The Master and chawkk6404'
+__version__ = '1.1.0'

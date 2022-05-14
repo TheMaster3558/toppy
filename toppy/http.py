407d759709ccccb437d7663e029fb341f924449e
chawkk6404 <chawk_k@outlook.com>
2022-05-13 18:40:15 -0700

    Use a base class for http clients

--- toppy/http.py
+++ toppy/http.py
@@ -1,15 +1,13 @@
 from __future__ import annotations
 
-from typing import Any, Optional, TYPE_CHECKING, TypeVar, Union
+import asyncio
+from typing import Any, Optional, TypeVar, Union
 import logging
 import time
 
 import aiohttp
 
 from .errors import *
-
-if TYPE_CHECKING:
-    import asyncio
 
 
 __all__ = (
@@ -25,12 +23,10 @@ K = TypeVar('K')
 V = TypeVar('V')
 
 
-
 def cleanup_params(params: dict[K, V]) -> dict[K, V]:
     return {k: v for k, v in params.items() if v is not None}
 
 
-# HTTP utils
 class RateLimiter:
     def __init__(self, rate: float, per: float):
         self.rate = rate
@@ -52,23 +48,31 @@ class RateLimiter:
             await asyncio.sleep(sleep_until)
 
 
-class DBLHTTPClient:
-    DBL_BASE = 'https://discordbotlist.com/api/v1'
-
-    def __init__(self, token, *, loop: Optional[asyncio.AbstractEventLoop] = None,
-                 session: Optional[aiohttp.ClientSession] = None):
-        self.token = token
-        self.session = session or aiohttp.ClientSession(loop=loop)
+class BaseHTTPClient:
+    BASE: str
+    token: str
+    session: aiohttp.ClientSession
 
     @property
-    def headers(self):
+    def headers(self) -> dict:
         return {'Authorization': self.token}
 
-    async def request(self, method: str, url: str, **kwargs: Any) -> aiohttp.ClientResponse:
-        resp = await self.session.request(method, self.DBL_BASE + url, **kwargs, headers=self.headers)
-        json = await resp.json()
+    async def check(self, url: str):
+        pass
 
-        _log.info(f'{resp.method} {resp.url} has returned with {resp.status} {json}')
+    async def request(self, method: str, url: str, **kwargs: Any) -> aiohttp.ClientResponse:
+        await self.check(url)
+        resp = await self.session.request(method, self.BASE + url, **kwargs, headers=self.headers)
+        data = await resp.json()
+
+        _log.info(
+            f'%s %s with %s has returned status %d with %s',
+            resp.method,
+            resp.url,
+            kwargs.get('data', kwargs.get('params')),
+            resp.status,
+            data
+        )
 
         if resp.ok:
             return resp
@@ -80,8 +84,17 @@ class DBLHTTPClient:
         elif resp.status == 403:
             raise Forbidden(resp)
         elif resp.status == 429:
-            raise RateLimited(json['retry-after'], resp)
+            raise RateLimited(data['retry-after'], resp)
         raise HTTPException()
+
+
+class DBLHTTPClient(BaseHTTPClient):
+    BASE = 'https://discordbotlist.com/api/v1'
+
+    def __init__(self, token, *, loop: Optional[asyncio.AbstractEventLoop] = None,
+                 session: Optional[aiohttp.ClientSession] = None):
+        self.token = token
+        self.session = session or aiohttp.ClientSession(loop=loop)
 
     async def post_stats(self, bot_id: int, /, *, voice_connections: int, users: int, guilds: int
                          ) -> aiohttp.ClientResponse:
@@ -95,8 +108,8 @@ class DBLHTTPClient:
             return resp
 
 
-class TopGGHTTPClient:
-    TOPGG_BASE = 'https://top.gg/api'
+class TopGGHTTPClient(BaseHTTPClient):
+    BASE = 'https://top.gg/api'
 
     def __init__(self, token, *, loop: Optional[asyncio.AbstractEventLoop] = None,
                  session: Optional[aiohttp.ClientSession] = None):
@@ -108,37 +121,13 @@ class TopGGHTTPClient:
             '/bots': RateLimiter(60, 60)
         }
 
-    @property
-    def headers(self):
-        return {'Authorization': self.token}
-
-    async def check(self, url: str):
+    async def check(self, url: str) -> None:
         k: str
         v: RateLimiter
 
         for k, v in self.rate_limits.items():
             if k in url:
                 await v.check()
-
-    async def request(self, method: str, url: str, **kwargs: Any) -> aiohttp.ClientResponse:
-        await self.check(url)
-        resp = await self.session.request(method, self.TOPGG_BASE + url, **kwargs, headers=self.headers)
-        json = await resp.json()
-
-        _log.info(f'{resp.method} {resp.url} has returned with {resp.status} {json}')
-
-        if resp.ok:
-            return resp
-
-        if resp.status == 400:
-            raise BadRequest(resp)
-        elif resp.status == 401:
-            raise Unauthorized(resp)
-        elif resp.status == 403:
-            raise Forbidden(resp)
-        elif resp.status == 429:
-            raise RateLimited(json['retry-after'], resp)
-        raise HTTPException()
 
     async def search_bots(self, search: str, *, limit: Optional[int] = None,
                           offset: Optional[int] = None) -> list[dict[str, Any]]:
@@ -172,4 +161,3 @@ class TopGGHTTPClient:
         })
         async with await self.request('POST', f'/bots/{bot_id}/stats', data=data) as resp:
             return resp
-
